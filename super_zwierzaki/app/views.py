3761abd7e0f9a1453f7f311f3995cc964cbb4d61
Joanna Byra <byra.joann@gmail.com>
2017-05-24 22:38:20 +0200

    displaying cards from database

--- super_zwierzaki/app/views.py
+++ super_zwierzaki/app/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template import loader
 
+from .models import Card
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    cards_list = Card.objects.all()
+    template = loader.get_template('app/index.html')
+    context = {
+        'cards_list' : cards_list,
+    }
+    return HttpResponse(template.render(context, request))
