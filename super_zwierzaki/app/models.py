3eedabf977f9ce3de454b936278594c22b9ec7bb
Joanna Byra <byra.joann@gmail.com>
2017-05-24 21:43:28 +0200

    database model

--- super_zwierzaki/app/models.py
+++ super_zwierzaki/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    user_id  = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+
+class Card(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    card_type = models.CharField(max_length=50)
+    card_number = models.IntegerField()
+    added_date = models.DateTimeField()
+    
+
+
+
