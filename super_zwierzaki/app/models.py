f3ed2422ba94d5a577585cb2c7d98ba8c96d22ed
Joanna Byra <byra.joann@gmail.com>
2017-05-25 12:54:20 +0200

    readme and database model update

--- super_zwierzaki/app/models.py
+++ super_zwierzaki/app/models.py
@@ -5,9 +5,13 @@ class User(models.Model):
     user_id  = models.CharField(max_length=50)
     location = models.CharField(max_length=50)
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, default="-")
+
 class Card(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    card_type = models.CharField(max_length=50)
+    card_type = models.ForeignKey(Category, on_delete=None)
+    card_name = models.CharField(max_length=50, default="-")
     card_number = models.IntegerField()
     added_date = models.DateTimeField()
     
