ea3bbea56ceff1efae412501b206739718a2482e
Joanna Byra <byra.joann@gmail.com>
2017-07-04 19:19:11 +0200

    login page added

--- super_zwierzaki/super_zwierzaki/urls.py
+++ super_zwierzaki/super_zwierzaki/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('app.urls'))
+    url(r'^', include('app.urls')),
+    url(r'^login/$', auth_views.login, {'template_name': 'app/login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'template_name': 'app/logout.html'}, name='logout')
 ]
